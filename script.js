ecc4a15ecc967e4c6b845a6ac8410a130dc75abd
alexmcbrier <alexmcbrier@icloud.com>
2024-05-30 17:18:02 -0400

    added a few inputs

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 let age = 10;
-    function updateAge() {
-        const ageDropdown = document.getElementById('ageDropdown');
-        age = ageDropdown.value;
-        document.getElementById('ageDisplay').innerText = 'Selected Age: ' + age;
-    }
+let maritalStatus = "married"
+let retirmentAge = 65
+function updateAge() {
+    const ageInput = document.getElementById('ageInput');
+    age = ageInput.value;
+    document.getElementById('ageDisplay').innerText = 'Selected Age: ' + age;
+}
+function updateMaritalStatus() {
+    const maritalDropdown = document.getElementById('maritalStatusDropdown');
+    maritalStatus = maritalDropdown.value;
+    document.getElementById('maritalDisplay').innerText = 'Marrige Status: ' + maritalStatus;
+}
+function updateRetirmentAge() {
+    const retirmentAgeInput = document.getElementById('retirmentAgeInput');
+    retirmentAge = retirmentAgeInput.value;
+    document.getElementById('retirmentAgeDisplay').innerText = 'Selected Retirment Age: ' + retirmentAge;
+}
